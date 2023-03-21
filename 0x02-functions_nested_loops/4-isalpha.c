1fa3a3797dee8bfa68046dd6e96bd4061878e98d
root <ibrahimmansour58563@gmail.com>
2023-03-21 08:28:07 -0700

    sd

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,22 +6,7 @@
 *
 * return: Always 0 (Success)
 */
-int _isalpha(int c);
-int main(void)
-{
-int r;
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
-_isalpha(int c)
+int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 {
