41cba11517594110e4bfb9be1f13f23bcfa78c21
root <ibrahimmansour58563@gmail.com>
2023-03-21 09:03:47 -0700

    5-try

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
-* main - start of the program Entry point
-*
-* Description: program that checks for letter c
-*
-* return: Always 0 (Success)
+* print_sign - print + or - or 0
+* @n: is a integer number
+* Return: 0 (Success)
 */
-int print_sign(int n){
-   if (n > 0)
-   {
-       return (1);
-       _putchar('+');
-   }
-   else if (n == 0)
-   {
-       return (0);
-      _putchar('0');
-   }
-   else
-   {
-       return (-1);
-      _putchar('-');
-   }
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+_putchar('+');
+}
+else if (n == 0)
+{
+return (0);
+_putchar('0');
+_putchar('0');
+}
+else
+{
+return (-1);
+_putchar('-');
+_putchar('-');
+}
 }
