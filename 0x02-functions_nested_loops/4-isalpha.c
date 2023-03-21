02b9c478bab1ca82b815f58df570b2e619dae27e
root <ibrahimmansour58563@gmail.com>
2023-03-21 12:00:49 -0700

    4-1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
-* main - start of the program Entry point
-*
-* Description: program that checks for letter c
-*
-* return: Always 0 (Success)
+* _isalpha - check for alphabetic character
+* @c: integer for check
+* Return: Always 0 (Success)
 */
 int _isalpha(int c)
 {
