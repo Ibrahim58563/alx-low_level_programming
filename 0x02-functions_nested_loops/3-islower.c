3e80bda0e3669d85ef79b1bb0458432acc51b3e2
root <ibrahimmansour58563@gmail.com>
2023-03-21 12:05:38 -0700

    3-1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
-* main - start of the program Entry point
-*
-* Description: program that checks for letter c
-*
-* return: Always 0 (Success)
+* _islower - start of the program Entry point
+* @c: integer for check
+* Return: Always 0 (Success)
 */
-int _islower(int c);
-_islower(int c)
+int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
