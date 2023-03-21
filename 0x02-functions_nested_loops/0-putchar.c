db1cba79034594ee391e4c1c49839eea2cef87d1
root <ibrahimmansour58563@gmail.com>
2023-03-21 05:29:26 -0700

    start

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main: start of the program Entry point
+ *
+ * Description: program that prints "_putchar", followed by a new line
+ *
+ * return: always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
