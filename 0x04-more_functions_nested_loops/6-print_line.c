24b4fcb6255858e7df293125396b921398fc95ab
root <ibrahimmansour58563@gmail.com>
2023-03-23 08:52:59 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line
+ * @n: input for length
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
